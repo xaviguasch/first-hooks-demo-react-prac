f3247aefc6ef12fac9970a91ae10abdae6ae07a9
xaviguasch <xaviguasch@gmail.com>
2019-05-25 16:00:59 +0200

    added CounterClass component

---
+++ src/CounterClass.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class CounterClass extends Component {
+  render() {
+    return (
+      <div>
+        <h1>The Count Is: </h1>
+        <button>Add 1</button>
+      </div>
+    )
+  }
+}
+
+export default CounterClass
