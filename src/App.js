bd646ede866abf3d288344ea539774f82ab6efd8
xaviguasch <xaviguasch@gmail.com>
2019-05-27 11:29:09 +0200

    added CounterHooks component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { Component } from 'react'
 import CounterClass from './CounterClass'
+import CounterHooks from './CounterHooks'
 import './App.css'
 
 function App() {
   return (
     <div className='App'>
       <CounterClass />
+      <CounterHooks />
     </div>
   )
 }
--- src/CounterClass.js
+++ src/CounterClass.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react'
 
 class CounterClass extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      count: 0
+    }
+
+    this.increment = this.increment.bind(this)
+  }
+
+  increment() {
+    this.setState({ count: this.state.count + 1 })
+  }
+
   render() {
     return (
       <div>
-        <h1>The Count Is: </h1>
-        <button>Add 1</button>
+        <h1>The Count Is: {this.state.count}</h1>
+        <button onClick={this.increment}>Add 1</button>
       </div>
     )
   }
