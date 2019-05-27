e0efe513c15a8e085457bbc97783105c0d4e5c6b
xaviguasch <xaviguasch@gmail.com>
2019-05-27 14:25:18 +0200

    added SimpleFormClass

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import CounterClass from './CounterClass'
 import CounterHooks from './CounterHooks'
 import Toggler from './Toggler'
+import SimpleFormClass from './SimpleFormClass'
 import './App.css'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <CounterClass />
       <CounterHooks />
       <Toggler />
+      <SimpleFormClass />
     </div>
   )
 }
--- src/Toggler.js
+++ src/Toggler.js
@@ -1,14 +1,16 @@
-import React, { useState } from 'react'
+import React from 'react'
+import useToggle from './hooks/useToggle'
 
 function Toggler() {
-  const [isHappy, setIsHappy] = useState(true)
-  const toggle = () => {
-    setIsHappy(!isHappy)
-  }
+  const [isHappy, toggleIsHappy] = useToggle(true)
+  const [isHeartBroken, toggleIsHeartBroken] = useToggle(false)
+  const [isBanana, toggleIsBanana] = useToggle(true)
 
   return (
     <div>
-      <h1 onClick={toggle}>{isHappy ? '😊' : '😞'}</h1>
+      <h1 onClick={toggleIsHappy}>{isHappy ? '😊' : '😞'}</h1>
+      <h1 onClick={toggleIsHeartBroken}>{isHeartBroken ? '💔' : '❤️'}</h1>
+      <h1 onClick={toggleIsBanana}>{isBanana ? '🍌' : '🙅‍'}</h1>
     </div>
   )
 }
